36dad292159e415936dde56897a34af3538c8d52
zainuleb <zainulebadd@gmail.com>
2024-04-26 17:39:42 +0200

    parking page added

--- src/pages/ParkingPage.js
+++ src/pages/ParkingPage.js
@@ -1,45 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const ParkingPage = () => {
+  // Set your target date and time
+  const targetDate = new Date("2024-06-01T23:59:59");
+
+  // State to store the remaining time
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  // Update the countdown
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = targetDate - now;
+
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+      setTimeLeft({ days, hours, minutes, seconds });
+
+      if (difference < 0) {
+        clearInterval(interval);
+        setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [targetDate]);
+
   return (
     <div className="marshall-container">
       <div className="marshall-col-5 marshall-col-screen marshall-screen-left">
-        <div className="marshall-logo fadeIn fast">
-          <img src="images/logo.svg" alt="Marshall Logo" />
-        </div>
         <div
           id="marshall-animate-area"
           data-hide="mrs-scaleDown"
           className="marshall-single-fit-thumb marshall-animate-content marshall-animate-content mrs-active marshall-fit-column"
-          style={{ backgroundImage: "url('images/v6/building.jpg')" }}
+          style={{ backgroundImage: "url('images/v6/viadrina-hg.jpg')" }}
         ></div>
-        <div className="marshall-social-column">
-          <p className="fadeIn fast">Stay in touch :</p>
-          <ul className="marshall-social-links">
-            <li>
-              <a className="fadeIn fast-child-1" href="/#" title="Facebook">
-                <i className="fa fa-facebook" aria-hidden="true"></i>
-              </a>
-            </li>
-            <li>
-              <a className="fadeIn fast-child-2" href="/#" title="Facebook">
-                <i className="fa fa-twitter" aria-hidden="true"></i>
-              </a>
-            </li>
-            <li>
-              <a className="fadeIn fast-child-3" href="/#" title="Facebook">
-                <i className="fa fa-google-plus" aria-hidden="true"></i>
-              </a>
-            </li>
-            <li>
-              <a className="fadeIn fast-child-4" href="/#" title="Facebook">
-                <i className="fa fa-linkedin" aria-hidden="true"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
       </div>
       <div className="marshall-col-7 marshall-col-content">
+        <div className="marshall-logo fadeIn fast">
+          <img src="images/logo-yellow.png" alt="MuSA Logo" />
+        </div>
         <div className="marshall-content css-center">
           <h2 className="single_large_title big_title fadeIn fast">
             Launch In
@@ -47,25 +57,9 @@ const ParkingPage = () => {
           <h2
             className="single_large_title fadeIn medium"
             id="countdown_text_layout"
-          ></h2>
-          <p className="short_description">
-            Be first to know about the latest updates and get exclusive offer on
-            our grand opening
-          </p>
-
-          <div className="default_mrs_newsletter marshall-inline-form">
-            <form id="marshall-form" className="marshall-newsletter-content">
-              <input
-                id="marshall-email"
-                type="email"
-                placeholder="Your Email"
-              />
-              <button className="marshall_submit" type="submit">
-                Subscribe
-              </button>
-              <label for="marshall-email"></label>
-            </form>
-          </div>
+          >
+            {`${timeLeft.days} days ${timeLeft.hours} hrs ${timeLeft.minutes} mins ${timeLeft.seconds} secs`}
+          </h2>
         </div>
       </div>
     </div>
